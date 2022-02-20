4b636cd26002dcc199c55a8f52a49a40f3051fc3
1Mill <erik.ekberg@outlook.com>
2022-02-20 14:27:48 -0800

    Add state to square

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 import { Component } from 'react'
 
 class Square extends Component {
+	constructor(props) {
+		super(props)
+		this.state = { value: null }
+	}
 	render() {
 		return (
 			<button
